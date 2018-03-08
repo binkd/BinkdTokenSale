5f6075beab9f637b628ff4b4a8dab75e332372fc
Mandy Singh <mandys@gmail.com>
2018-03-08 13:10:11 +0530

    break presale into two

---
+++ contracts/BinkdPrivatesale.sol
@@ -0,0 +1,91 @@
+pragma solidity ^0.4.18;
+
+import './BinkdToken.sol';
+
+import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
+import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
+
+/**
+ * @title BinkdPrivatesale - This is the private sale contract for Binkd Project
+ * @author Mandy Singh
+ * @dev BinkdPrivatesale is a Capped Crowdsale
+ * The presale is also Pausable
+ * There will be a max cap of 4000 ethers for the presale after which no investments would be accepted.
+ * Tokens offered through privatesale, presale and crowdsale would be 250mn (40%)
+ * 40% would be at the end of crowdsale and transferred to the Binkd Token Reserve
+ * Private sale participants would be offered 50% extra tokens
+ * Minimum investment amount during presale would be 5 ether with no maximum limit
+ */
+contract BinkdPrivatesale is CappedCrowdsale, Pausable {
+
+  uint256 public minimalInvestmentInWei = 5 ether;
+  address public tokenAddress;
+
+  BinkdToken public binkdToken;
+
+  event InitialDateReset(uint256 startTime, uint256 endTime);
+  event InitialRateChange(uint256 rate, uint256 cap, uint256 minimalInvestment);
+
+  function BinkdPrivatesale(uint256 _cap, uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet, address _tokenAddress) 
+    CappedCrowdsale(_cap)
+    Crowdsale(_startTime, _endTime, _rate, _wallet) public {
+      tokenAddress = _tokenAddress;
+      token = createTokenContract();
+    }
+
+  function createTokenContract() internal returns (MintableToken) {
+    binkdToken = BinkdToken(tokenAddress);
+    return BinkdToken(tokenAddress);
+  }  
+
+  // overriding Crowdsale#validPurchase to add extra cap logic
+  // @return true if investors can buy at the moment
+  function validPurchase() internal view returns (bool) {
+    bool minimalInvested = msg.value >= minimalInvestmentInWei;
+
+    return super.validPurchase() && minimalInvested && !paused;
+  }
+
+  /**
+    * @dev Reset start and end date/time for this Presale.
+    * @param _startTime change presale start time
+    * @param _endTime change presale end period
+    */
+  function setPrivateSaleDates(uint256 _startTime, uint256 _endTime) public onlyOwner returns (bool) { 
+      require(startTime > block.timestamp);
+      require(_startTime >= now);
+      require(_endTime >= _startTime);
+
+      startTime = _startTime;
+      endTime = _endTime;
+
+      InitialDateReset(startTime, endTime);
+      return true;
+  }
+
+  /**
+    * @dev Sets the token conversion rate
+    * @param _rateInWei - Price of 1 Binkd token in Wei. 
+    * @param _capInWei - Cap of the Presale in Wei. 
+    * @param _minimalInvestmentInWei - Minimal investment in Wei. 
+    */
+  function setRate(uint256 _rateInWei, uint256 _capInWei, uint256 _minimalInvestmentInWei) public onlyOwner returns (bool) { 
+      require(startTime >= block.timestamp); // can't update anymore if sale already started
+      require(_rateInWei > 0);
+      require(_capInWei > 0);
+      require(_minimalInvestmentInWei > 0);
+
+      rate = _rateInWei;
+      cap = _capInWei;
+      minimalInvestmentInWei = _minimalInvestmentInWei;
+
+      InitialRateChange(rate, cap, minimalInvestmentInWei);
+      return true;
+  }
+
+  // set the token owner to contract owner
+  function reTransferTokenOwnership() onlyOwner public { 
+      binkdToken.transferOwnership(owner);
+  }
+  
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var BinkdToken   = artifacts.require("BinkdToken.sol");
-var BinkdPresale = artifacts.require("BinkdPresale.sol");
+var BinkdPrivatesale = artifacts.require("BinkdPrivatesale.sol");
 
 const duration = {
     seconds: function (val) { return val; },
@@ -23,29 +23,29 @@ module.exports = function(deployer, network, accounts) {
         maxTokenSupply = 1000e18; //1000 is maxTokenSupply
         presaleCap     = 2000e18;
     } else {
-        startTime      = 1518282000; //Saturday February 10, 2018 12:00:00 (pm) EST
-        endTime        = 1519491600; //Saturday February 24, 2018 12:00:00 (pm) EST
+        startTime      = 1520335800; //Tuesday March 06, 2018 11:30:00 (am) UTC
+        endTime        = 1523014200; //Friday April 06, 2018 11:30:00 (am) UTC
         ethRate        = new web3.BigNumber(13300); //@1000USD to ETH, with 33% bonus
         wallet         = accounts[0];
         //625mn is a finite no of tokens that will not increase
         maxTokenSupply = 625000000e18; //max supply of 625mn tokens
-        presaleCap     = 1500e18; //raise a max of 1500 ether @1000USD conversion, that's 2mn USD        
+        presaleCap     = 4000e18; //raise a max of 1500 ether @1000USD conversion, that's 2mn USD        
     }
     // deployer.deploy(BinkdToken, maxTokenSupply).then(function() {
-    //     return deployer.deploy(BinkdPresale, presaleCap, startTime, endTime, ethRate, wallet, BinkdToken.address);
+    //     return deployer.deploy(BinkdPrivatesale, presaleCap, startTime, endTime, ethRate, wallet, BinkdToken.address);
     // });  
     deployer.then(function() {
         return deployer.deploy(BinkdToken, maxTokenSupply);
     }).then(function() { 
         console.log("BinkdToken address is : " + BinkdToken.address);
-        return deployer.deploy(BinkdPresale, presaleCap, startTime, endTime, ethRate, wallet, BinkdToken.address);
+        return deployer.deploy(BinkdPrivatesale, presaleCap, startTime, endTime, ethRate, wallet, BinkdToken.address);
     }).then(function() {
-        console.log("BinkdPresale address is : " + BinkdPresale.address);
+        console.log("BinkdPrivatesale address is : " + BinkdPrivatesale.address);
         return BinkdToken.deployed();
     }).then(function(instance) { 
         console.log("BinkdToken instance is : " + instance.address);
         console.log("Transfering ownership to presale contract ...");
-        return instance.transferOwnership(BinkdPresale.address, {from: accounts[0]});
+        return instance.transferOwnership(BinkdPrivatesale.address, {from: accounts[0]});
     });
 
 };
